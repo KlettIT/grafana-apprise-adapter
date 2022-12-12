1272006de9bea567861fdd1e5b44845f64cbb61f
Klett IT <71817167+KlettIT@users.noreply.github.com>
2022-12-12 13:34:46 +0100

    Update grafana.rs

--- src/grafana.rs
+++ src/grafana.rs
@@ -14,21 +14,10 @@ pub enum GrafanaState {
 
 #[derive(Deserialize, Debug)]
 pub struct GrafanaPayload {
-    #[serde(alias = "dashboardId")]
-    pub dashboard_id: u64,
-    // Note(andrew): 'message' field is not always present.
     pub message: Option<String>,
     #[serde(alias = "orgId")]
     pub org_id: u64,
-    #[serde(alias = "panelId")]
-    pub panel_id: u64,
-    #[serde(alias = "ruleId")]
-    pub rule_id: u64,
-    #[serde(alias = "ruleName")]
-    pub rule_name: String,
-    #[serde(alias = "ruleUrl")]
-    pub rule_url: String,
+    pub status: String,
     pub state: GrafanaState,
-    pub tags:HashMap<String, String>,
     pub title: String,    
 }
